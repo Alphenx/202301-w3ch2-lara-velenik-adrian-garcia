f72e3ce7835836e044865c1feae05085ea33548a
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-01-31 14:07:30 +0100

    Initial commit

---
+++ public/js/demo.js
@@ -0,0 +1,10 @@
+import { hello } from './hello.js';
+const numberOfChild = 45;
+hello.charAt(0);
+if (numberOfChild > 3) {
+    console.log('hola');
+    for (let i = 0; i < 100; i++) {
+        console.log('adios');
+    }
+}
+export const demoTest = 'Hello';
---
+++ src/types/character.model.ts
@@ -0,0 +1,6 @@
+export interface Character {
+  name: string;
+  family: string;
+  age: number;
+  status: string;
+}
